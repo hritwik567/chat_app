e2a169c893522224ed6b1fbfae62c4c36f8dd0e5
hritvik taneja <hritviktaneja@HRITVIKs-MacBook-Air.local>
2017-03-22 22:36:20 +0530

    made an IRC atleast I think so and added users feature to it

--- server.js
+++ server.js
@@ -5,18 +5,37 @@ var io = require('socket.io')(server);
 app.get('/',function (req,res) {
 	res.sendFile(__dirname +'/index.html');
 });
-
+var users = [] , connections = []; 
 io.on('connection',function(socket){
-	console.log('connected to a user');
+	connections.push(socket);
+	console.log('connected to %s users',connections.length);
+
+
 	socket.on('disconnect',function(){
-		console.log('a user got disconnected');
+		connections.splice(connections.indexOf(socket),1);
+		users.splice(users.indexOf(socket.username),1);
+		updateUsers();
+		io.emit('chat message',{msg: socket.username +' disconnected', username:socket.username});
+		console.log('Disconnected: Now only %s users connnected',connections.length);
 	});
 
-	socket.on('chat message',function(msg){
+	socket.on('chat message',function(data){
+		console.log(data);
+		io.emit('chat message',{msg:data , username:socket.username});
+	});
+
+	socket.on('new user',function(msg){
+		socket.emit('userSucess',msg);
 		console.log(msg);
-		io.emit('chat message',msg);
+		socket.username = msg;
+		users.push(socket.username);
+		updateUsers();
 	});
 });
+
+function updateUsers() {
+	io.emit('get user',users);
+}
 
 server.listen(8888,function(){
 	console.log('connected to server...');
